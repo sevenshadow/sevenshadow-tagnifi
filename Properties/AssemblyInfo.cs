df7dc7cecdf1f1e020f53f2352f9c115735080a7
Charlie Strout <charlie@sevenshadow.com>
2017-01-04 12:56:36 -0500

    Update assembly version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SevenShadow.TagniFi")]
 [assembly: AssemblyDescription("Code to call the Tagnifi API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Seven Shadow LLC")]
 [assembly: AssemblyProduct("SevenShadow.TagniFi")]
 [assembly: AssemblyCopyright("Copyright Seven Shadow LLC 2015 -2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Seven Shadow LLC")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
